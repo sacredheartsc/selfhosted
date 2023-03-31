523e0c3cc6163b7bd4b3003bbff0d6efda5eea87
Stonewall Jackson <stonewall@sacredheartsc.com>
2023-03-31 10:35:42 -0400

    local_homedirs: add systemd environment generators

---
+++ roles/local_homedirs/files/etc/systemd/user-environment-generators/local-homedirs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eu
+
+uid=$(id -u)
+username=$(id -un)
+
+if (( uid >= 1000 )); then
+  echo "XDG_DATA_HOME=/usr/local/home/${username}/.local/share"
+  echo "XDG_STATE_HOME=/usr/local/home/${username}/.local/state"
+  echo "XDG_CACHE_HOME=/usr/local/home/${username}/.cache"
+  echo "XDG_CONFIG_HOME=/usr/local/home/${username}/.config"
+fi
